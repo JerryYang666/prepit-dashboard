53d57f1f5bde566e2c90c9034a002ab433ca7cdf
Ruihuang Yang <rxy216@case.edu>
2024-04-09 02:44:57 -0400

    Update avatar images in recent-sales.tsx

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -1,11 +1,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+const AvatarGenerator = () => {
+  return `https://api.dicebear.com/8.x/thumbs/svg?seed=${Math.random().toString()}`;
+}
+
 export function RecentSales() {
   return (
     <div className="space-y-8">
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/01.png" alt="Avatar" />
+          <AvatarImage src={AvatarGenerator()} alt="Avatar" />
           <AvatarFallback>OM</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -18,7 +22,7 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
-          <AvatarImage src="/avatars/02.png" alt="Avatar" />
+          <AvatarImage src={AvatarGenerator()} alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -29,7 +33,7 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/03.png" alt="Avatar" />
+          <AvatarImage src={AvatarGenerator()} alt="Avatar" />
           <AvatarFallback>IN</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -42,7 +46,7 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/04.png" alt="Avatar" />
+          <AvatarImage src={AvatarGenerator()} alt="Avatar" />
           <AvatarFallback>WK</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -53,7 +57,7 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/05.png" alt="Avatar" />
+          <AvatarImage src={AvatarGenerator()} alt="Avatar" />
           <AvatarFallback>SD</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
