c3732694fc65ff4a9e9ec01eeb13581da32c778e
Ruihuang Yang <rxy216@case.edu>
2024-06-12 13:09:29 -0400

    feat: Add middleware for authentication and redirection

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,69 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const signInPagePath = "/auth/signin";
+const dashboardPagePath = "/dashboard";
+const basePath = "/";
+
 export function middleware(request: NextRequest) {
-  console.log(request.nextUrl.pathname);
-  const allCookies = request.cookies.getAll();
-  console.log(allCookies);
+  if (request.nextUrl.pathname === basePath) {
+    const refreshToken = request.cookies.get("refresh_token")?.value;
+    // check if refresh token is an uuid
+    const isUuid =
+      refreshToken &&
+      refreshToken.match(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+      );
+    const isLoggedin = refreshToken && isUuid;
+    if (isLoggedin) {
+      // redirect to the dashboard
+      const url = request.nextUrl.clone();
+      url.pathname = dashboardPagePath;
+      return NextResponse.redirect(url);
+    } else {
+      // redirect to the sign in page
+      const url = request.nextUrl.clone();
+      url.pathname = signInPagePath;
+      return NextResponse.redirect(url);
+    }
+  }
+
+  if (request.nextUrl.pathname === signInPagePath) {
+    const refreshToken = request.cookies.get("refresh_token")?.value;
+    // check if refresh token is an uuid
+    const isUuid =
+      refreshToken &&
+      refreshToken.match(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+      );
+    const isLoggedin = refreshToken && isUuid;
+    if (isLoggedin) {
+      // redirect to the dashboard
+      const url = request.nextUrl.clone();
+      url.pathname = dashboardPagePath;
+      return NextResponse.redirect(url);
+    } else {
+      return NextResponse.next();
+    }
+  }
+
+  if (request.nextUrl.pathname.startsWith(dashboardPagePath)) {
+    const refreshToken = request.cookies.get("refresh_token")?.value;
+    // check if refresh token is an uuid
+    const isUuid =
+      refreshToken &&
+      refreshToken.match(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+      );
+    const isLoggedin = refreshToken && isUuid;
+    if (isLoggedin) {
+      return NextResponse.next();
+    } else {
+      // redirect to the sign in page
+      const url = request.nextUrl.clone();
+      url.pathname = signInPagePath;
+      return NextResponse.redirect(url);
+    }
+  }
   return NextResponse.next();
 }
