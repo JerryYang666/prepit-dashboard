7ae97df84908fe7f5ddc8032ef7ec6b1c644ce88
Ruihuang Yang <rxy216@case.edu>
2024-06-23 02:32:28 -0400

    refactor: Update prepitInterviewPageUrl constant to use the correct URL

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,2 +1,2 @@
 export const lastUsedLoginProviderLocalStorageKey = "prepit.ai-last-used-login-provider";
-export const prepitInterviewPageUrl = "https://test-app.prepit.ai/loading";
+export const prepitInterviewPageUrl = "https://interview.prepit.ai/loading";
